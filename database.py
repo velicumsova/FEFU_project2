b9362b7d167882e142002d59410082f5c86b31fb
Gleb <m_marple1@mail.ru>
2023-04-18 23:41:24 +1000

    Made 4 functions to work with db

--- database.py
+++ database.py
@@ -7,59 +7,13 @@ import data.config
 class Database:
     def __init__(self, path_to_db = data.config.path_to_database):
         self.path_to_db = path_to_db
-        self.create_table_of_user()
+        self.create_table_of_rooms()
+        self.create_table_of_users()
+
 
     @property
     def connection(self):
         return sqlite3.connect(self.path_to_db)
-
-    def create_table_of_user(self):
-        sql = """
-        create table `users` (
-          `user_id` AUTOINCREMENT not null,
-          `first_name` VARCHAR(255) not null,
-          `last_name` VARCHAR(255) not null,
-          `login` VARCHAR(255) not null,
-          `password` VARCHAR(255) not null,
-          `login_status` VARCHAR(255) not null,
-
-          primary key (`user_id`)
-    )"""
-        self.execute(sql, commit=True)
-
-    def user_login(self, login, password):
-        sql = "SELECT * FROM users WHERE login = ? and password = ?"
-        result = self.execute(sql, (login, password), fetchone=True)
-
-        if result is not None:
-            sql_update = "UPDATE users SET login_status='active' WHERE user_id=?"
-            self.execute(sql_update, (result.user_id,), commit=True)
-            print(f"Вы успешно вошли в аккаунт")
-        else:
-            print(f"Пользователя с такими данными не существует")
-
-    def user_register(self, login, password, first_name, last_name):
-        sql = "SELECT * FROM users WHERE login=?"
-        result = self.execute(sql, (login,), fetchone=True)
-
-        if result is not None:
-            print("Пользователь с таким именем уже зарегистрирован")
-        else:
-            sql_insert = "INSERT INTO users (login, password, first_name, last_name, login_status) VALUES (?, ?, ?, ?, ?)"
-            self.execute(sql_insert, (login, password, first_name, last_name, 'active'), commit=True)
-            print("Вы успешно зарегистрировались")
-
-    def login_status(self, login):
-        sql = "SELECT login_status FROM users WHERE login=?"
-        result = self.execute(sql, (login,), fetchone=True)
-        if result is not None:
-            status = result[0]
-            return status
-        else:
-            return None
-
-    def get_rooms_list(self):
-
 
     def execute(self, sql: str, parameters: tuple = None, fetchone=False, fetchall=False, commit=False):
         if not parameters:
@@ -68,7 +22,7 @@ class Database:
         connection = self.connection
         # connection.set_trace_callback(logger)
         cursor = connection.cursor()
-        cursor.execute(sql, parameters)
+        # cursor.execute(sql, parameters)
         data = None
 
         if commit:
@@ -81,72 +35,93 @@ class Database:
 
         return data
 
-    def create_table_of_user(self):
+    def create_table_of_rooms(self):
+        sql = """
+        create table `rooms` (
+          `room_floor` INT8 not null,
+          `room_number` INT8 not null,
+          `occupied` BOOLEAN null,
+          `room_resident` varchar(255) not null,
+          `room_id` INTEGER PRIMARY KEY not null
+    )"""
+        self.execute(sql, commit=True)
+
+    def create_table_of_users(self):
         sql = """
         create table `users` (
-          `user_id` AUTOINCREMENT not null,
+          `user_id` INTEGER PRIMARY KEY not null,
           `first_name` VARCHAR(255) not null,
           `last_name` VARCHAR(255) not null,
           `login` VARCHAR(255) not null,
           `password` VARCHAR(255) not null,
-          `login_status` VARCHAR(255) not null,
-          
-          primary key (`user_id`)
+          `login_status` VARCHAR(255) not null
     )"""
         self.execute(sql, commit=True)
 
-    def add_user(self, telegram_id, telegram_username):
-        print(telegram_id)
-        print(telegram_username)
-        registration_date = datetime.datetime.now().strftime('%H:%M:%S %d.%m.%y')
-        user_status = 'OK'
-        is_new_user = 1
+    def user_login(self, login, password):
+        sql = "SELECT * FROM users WHERE login = ? and password = ?"
+        result = self.execute(sql, (login, password), fetchone=True)
 
-        result = self.select_one_user(telegram_id=telegram_id)
         if result is not None:
-            print(f'Пользователь {telegram_id} - уже существует! Его нельзя зарегистрировать!')
-            return f'Пользователь {telegram_id} - уже существует! Его нельзя зарегистрировать!'
+            sql_update = "UPDATE users SET login_status='active' WHERE user_id=?"
+            self.execute(sql_update, (result.user_id,), commit=True)
+            return json.dumps({
+                'server_answer': f"Вы успешно вошли в аккаунт",
+                'answer_status': 'ok'
+            })
+        else:
+            return json.dumps({
+                'server_answer': f"Пользователя с такими данными не существует",
+                'answer_status': 'ok'
+            })
 
-        sql = "INSERT INTO All_Users(telegram_id, telegram_username, registration_date, user_status)" \
-              " VALUES(?, ?, ?, ?)"
-        parameters = (telegram_id, telegram_username, registration_date, user_status)
-        self.execute(sql, parameters=parameters, commit=True)
-        print(f'Пользователь {telegram_id} - успешно добавлен!')
-        return f'Пользователь {telegram_id} - успешно добавлен!'
+    def user_register(self, login, password, first_name, last_name):
+        sql = "SELECT * FROM users WHERE login=?"
+        result = self.execute(sql, (login,), fetchone=True)
 
-    def select_all_users(self):
-        sql = 'SELECT * FROM All_Users'
-        return self.execute(sql, fetchall=True)
+        if result is not None:
+            return json.dumps({
+                'server_answer': f"Пользователь с таким именем уже зарегистрирован",
+                'answer_status': 'ok'
+            })
+        else:
+            sql_insert = "INSERT INTO users (login, password, first_name, last_name, login_status) VALUES (?, ?, ?, ?, ?)"
+            self.execute(sql_insert, (login, password, first_name, last_name, 'active'), commit=True)
+            print("Вы успешно зарегистрировались")
+            return json.dumps({
+                'server_answer': f"Вы успешно зарегистрировались",
+                'answer_status': 'ok'
+            })
 
-    @staticmethod
-    def format_args(sql, parameters: dict):
-        # используется для создания sql команды с нужными параметрами для команды ниже
-        sql += ' AND '.join([
-            f"{item} = ?" for item in parameters.keys()
-        ])
-        return sql, tuple(parameters.values())
+    def login_status(self, login):
+        sql = "SELECT login_status FROM users WHERE login=?"
+        result = self.execute(sql, (login,), fetchone=True)
+        if result is not None:
+            status = result[0]
+            return status
+        else:
+            return None
 
-    def select_one_user(self, **kwargs):
-        sql = 'SELECT * FROM All_Users WHERE '
-        sql, parameters = self.format_args(sql, kwargs)
-        return self.execute(sql, parameters, fetchone=True)
-        # пример использования команды select_user(id=131, name='JoJo')
+    def get_rooms_list(self):
+        sql = "SELECT * FROM rooms"
+        result = self.execute(sql, fetchall=True)
 
-    def select_many_users(self, **kwargs):
-        sql = 'SELECT * FROM All_Users WHERE '
-        sql, parameters = self.format_args(sql, kwargs)
-        return self.execute(sql, parameters, fetchall=True)
-        # пример использования команды select_user(id=131, name='x')
+        rooms = []
 
-    def update_any_info_about_line(self, user_id, thing_to_change, new_data):
-        result = self.select_one_user(telegram_id=user_id)
-        if not result:
-            print('Пользователя не существует, проверьте правильность id')
-            return 'Пользователя не существует, проверьте правильность id'
+        for item in result:
+            rooms.append({
+                'room_floor':item[0],
+                'room_number':item[1],
+                'occupied': bool(item[2]),
+                'room_resident':item[3]
+            })
 
-        sql = f"UPDATE All_Users SET {thing_to_change}=? WHERE telegram_id=?"
-        self.execute(sql, parameters=(new_data, user_id), commit=True)
+            return json.dumps({
+                'server_answer':'Список комнат',
+                'rooms': rooms
+            })
 
-
-if __name__ == '__main__':
-    pass
+# if __name__ == '__main__':
+#     pass
+db = Database()
+db.user_register("zxc", "123", "Gleb", "Kim")
